b830d83be80857d25d26eb163177791ddb9039c4
AL-FAHAMI <altoihir@gmail.com>
2024-05-08 18:27:10 +0100

    test: consutructor, setters w argument exception

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ import model.Member;
 public class Main {
 
     public static void main(String[] args) {
-        
+        Member admin = new Member(1, "TEST", "Shakur", "21/12/1992", "tupAC@gmail.com", null, 20111473, 1, 1, 1, 1, 1, true);
+
+        System.out.println(admin.toString());
     }
 }

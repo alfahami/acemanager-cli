ec1c54a6becff1267899eb8cb80df72046fe2bc1
AL-FAHAMI <altoihir@gmail.com>
2024-05-08 18:27:24 +0100

    Revert "test: consutructor, setters w argument exception"
    This reverts commit b830d83be80857d25d26eb163177791ddb9039c4.

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ import model.Member;
 public class Main {
 
     public static void main(String[] args) {
-        Member admin = new Member(1, "TEST", "Shakur", "21/12/1992", "tupAC@gmail.com", null, 20111473, 1, 1, 1, 1, 1, true);
-
-        System.out.println(admin.toString());
+        
     }
 }

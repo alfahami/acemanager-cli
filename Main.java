493c5560333f6303f110a317388b1c9118da6113
AL-FAHAMI <altoihir@gmail.com>
2024-05-08 18:49:29 +0100

    test: toString() overriding

--- Main.java
+++ Main.java
@@ -3,8 +3,15 @@ import model.Member;
 public class Main {
 
     public static void main(String[] args) {
-        Member admin = new Member(1, "TEST", "Shakur", "21/12/1992", "tupAC@gmail.com", null, 20111473, 1, 1, 1, 1, 1, true);
+        Member admin = new Member(1, "TEST", "Shakur", "21/12/1992", "tupac@gmail.com", "NBE388507", 20111473, 1, 1, 1, 1, 1, true);
 
+        Member shakur = new Member(1, "TEST", "Shakur", "21/12/1992", "tupac@gmail.com", "NBE388507", 20111473, 1, 1, 1, 1, 1, true);
+
+
+        Member moderator = admin;
+        System.out.println(moderator==admin);
+        System.out.println(admin==shakur);
         System.out.println(admin.toString());
+        System.out.println(moderator.toString());
     }
 }

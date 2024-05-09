d2c23db14385261d1ec1322e31f6e11536e6383d
AL-FAHAMI <altoihir@gmail.com>
2024-05-09 11:28:34 +0100

    fix: prefer using java.time over SimpleDateFormat

--- lib/Helpers.java
+++ lib/Helpers.java
@@ -1,10 +1,8 @@
 package lib;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Date;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,19 +16,13 @@ public class Helpers {
         return matcher.matches();
     }
 
-    // check Java 8 Docs for Date and SimpleFormat
+    // prefer java.time over SimpleFormat    
     public static boolean isValidDateFormat(String format, String value) {
-        Date date = null;
         try {
-            Locale.setDefault(Locale.FRANCE);
-            SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
-            date = sdf.parse(value);
-            if (!value.equals(sdf.format(date))) {
-                date = null;
-            }
-        } catch (ParseException | DateTimeParseException ex) {
-            throw new DateTimeParseException("Invalid date format", value, 6);
+            LocalDate.parse(value, DateTimeFormatter.ofPattern(format));
+            return true;
+        } catch ( DateTimeParseException ex) {
+            throw new IllegalArgumentException("Invalid date format");
         }
-        return date != null;
     }
 }

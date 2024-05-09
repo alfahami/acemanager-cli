51ed82039bcf4eda43c05850c95d8056e2049676
AL-FAHAMI <altoihir@gmail.com>
2024-05-09 17:52:18 +0100

    test: date validation

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import model.Member;
 public class Main {
 
     public static void main(String[] args) {
-        Member admin = new Member(1, "TEST", "Shakur", "", "tupac@gmail.com", "NBE388507", 20111473, 1, 1, 1, 1, 1, true);
+        Member admin = new Member(1, "TEST", "Shakur", "123/09/2012", "tupac@gmail.com", "NBE388507", 20111473, 1, 1, 1, 1, 1, true);
         Field fes = new Field(0, 1, "SMI", "LF", 3);
         Field fesCopy = new Field(fes);
 

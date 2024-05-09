33ce9cdf752d62d0466487a73c02cd0d79de2263
AL-FAHAMI <altoihir@gmail.com>
2024-05-09 16:11:23 +0100

    feat: add Faculty class

--- model/Faculty.java
+++ model/Faculty.java
@@ -1,5 +1,63 @@
 package model;
 
+import java.util.Objects;
+
 public class Faculty {
-    
+    private int id;
+    private String name;
+
+    public Faculty(int id, String name) {
+        setId(id);
+        setName(name);
+    }
+
+    public Faculty(Faculty source) {
+        setId(source.id);
+        setName(source.name);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        if (id <= 0)
+            throw new IllegalArgumentException("Faculty ID must be greater than 0");
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("Faculty name cannot be null or blank");
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "[" +
+                " ID='" + getId() + "'" +
+                ", Name='" + getName() + "'" +
+                "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Faculty)) {
+            return false;
+        }
+        Faculty faculty = (Faculty) o;
+        return id == faculty.id && Objects.equals(name, faculty.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
 }

d94a184a1237869a6097259c44a274198b4cf5f9
AL-FAHAMI <altoihir@gmail.com>
2024-05-09 17:45:24 +0100

    test: null, blank birthdate

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 import model.Field;
+import model.Member;
 
 public class Main {
 
     public static void main(String[] args) {
+        Member admin = new Member(1, "TEST", "Shakur", "", "tupac@gmail.com", "NBE388507", 20111473, 1, 1, 1, 1, 1, true);
         Field fes = new Field(0, 1, "SMI", "LF", 3);
         Field fesCopy = new Field(fes);
 

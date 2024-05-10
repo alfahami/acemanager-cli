2862b1ffa843a1b626068c27992636f8134be0e8
AL-FAHAMI <altoihir@gmail.com>
2024-05-10 18:01:40 +0100

    feat: add get single member interface manager

--- model/manager/Acemanager.java
+++ model/manager/Acemanager.java
@@ -5,8 +5,9 @@ import model.Member;
 public interface Acemanager {
 
     public void addMember(Member newMember);
-    public void removeMemeber(int index, Member member);
+    public void removeMemeber(int index);
     public void updateMemeber(int index, Member newMember);
     public Member findMember(String passport);
-    
+    public Member getMember(int index);
+
 }

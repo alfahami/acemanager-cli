7ca70c387e8f8c724b7a8ddf7ededf899d8efce4
AL-FAHAMI <altoihir@gmail.com>
2024-05-10 23:27:57 +0100

    refractor: add fill tables class

--- model/manager/Moderator.java
+++ model/manager/Moderator.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.lang.NullPointerException;
 
 import model.Member;
 

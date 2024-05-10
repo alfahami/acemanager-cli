fe6dda5664d8c886570120e3fa7e3c3b20702861
AL-FAHAMI <altoihir@gmail.com>
2024-05-10 18:27:12 +0100

    feat: override toString, equals, hash methods

--- model/manager/Moderator.java
+++ model/manager/Moderator.java
@@ -3,6 +3,7 @@ package model.manager;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import model.Member;
 
@@ -22,7 +23,8 @@ public class Moderator extends Member implements Acemanager {
     }
 
     public void setSession_start() {
-        this.session_start = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy' at 'HH:mm:ss")).toString();
+        this.session_start = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy' at 'HH:mm:ss"))
+                .toString();
     }
 
     public String getSession_end() {
@@ -30,7 +32,8 @@ public class Moderator extends Member implements Acemanager {
     }
 
     public void setSession_end() {
-        this.session_end = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy' at 'HH:mm:ss")).toString();
+        this.session_end = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy' at 'HH:mm:ss"))
+                .toString();
     }
 
     @Override
@@ -40,34 +43,66 @@ public class Moderator extends Member implements Acemanager {
 
     @Override
     public void updateMemeber(int index, Member newMember) {
-        if(index < 0 || index > this.members.size()) throw new IllegalArgumentException("Error: index " + index + "out of bounds");
+        if (index < 0 || index > this.members.size())
+            throw new IllegalArgumentException("Error: index " + index + "out of bounds");
         this.members.set(index, new Member(newMember));
     }
 
     @Override
     public void removeMemeber(int index) {
-        if(index < 0 || index > this.members.size()) throw new IllegalArgumentException("Error: index " + index + "out of bounds");
+        if (index < 0 || index > this.members.size())
+            throw new IllegalArgumentException("Error: index " + index + "out of bounds");
         this.members.remove(index);
     }
 
     @Override
     public Member findMember(String passport) {
-        if(passport == null || passport.isBlank()) throw new IllegalArgumentException("Passport number cannot be null or blank");
-        //int count = 0;
-        if(this.members == null) return null;
+        if (passport == null || passport.isBlank())
+            throw new IllegalArgumentException("Passport number cannot be null or blank");
+        // int count = 0;
+        if (this.members == null)
+            return null;
         else {
             for (Member member : this.members) {
-                if(member.getPassport().equals(passport)) {
+                if (member.getPassport().equals(passport)) {
                     return member;
                 }
-            }   
+            }
         }
-        //if(count == 0) return null;
+        // if(count == 0) return null;
         return null;
     }
 
     public Member getMember(int index) {
-        if(index < 0 || index > this.members.size()) throw new IllegalArgumentException("Error: index " + index + "out of bounds");
+        if (index < 0 || index > this.members.size())
+            throw new IllegalArgumentException("Error: index " + index + "out of bounds");
         return new Member(this.members.get(index));
     }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " session start='" + getSession_start() + "'" +
+                ", session end='" + getSession_end() + "'" +
+                ", members='" + members.toString() + "'" +
+                "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Moderator)) {
+            return false;
+        }
+        Moderator moderator = (Moderator) o;
+        return Objects.equals(session_start, moderator.session_start)
+                && Objects.equals(session_end, moderator.session_end) && Objects.equals(members, moderator.members);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(session_start, session_end, members);
+    }
+
 }

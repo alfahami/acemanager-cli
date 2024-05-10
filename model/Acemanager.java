90fa27423a3a18c959af7f3e4a8899e78ab7ad09
AL-FAHAMI <altoihir@gmail.com>
2024-05-10 16:40:29 +0100

    fix: add acemanager interface

--- model/Acemanager.java
+++ model/Acemanager.java
@@ -1,15 +1,10 @@
 package model;
 
-import java.util.ArrayList;
+public interface Acemanager {
 
-public abstract class Acemanager {
-    private String session_start;
-    private String session_end;
-    private ArrayList<Member> members;
-
-    public void addMember(Member newMember) {}
-    public void removeMemeber(int index, Member member) {}
-    public void updateMemeber(int index, Member newMember) {}
-    public Member findMember(String passport) { return null; }
+    public void addMember(Member newMember);
+    public void removeMemeber(int index, Member member);
+    public void updateMemeber(int index, Member newMember);
+    public Member findMember(String passport);
     
 }

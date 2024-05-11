8ae02308c47f45ff08399f7f1417ac0fee4e870f
AL-FAHAMI <altoihir@gmail.com>
2024-05-11 09:17:04 +0100

    feat: fill base tables

--- lib/FillTables.java
+++ lib/FillTables.java
@@ -1,5 +1,24 @@
 package lib;
 
+import model.City;
+import model.Faculty;
+import model.Field;
+
 public class FillTables {
+
+    private final static void fillFields() {
+        Field smi = new Field(1, 2, "SMI", "Mathematic and Computer Science", "LF", 3);
+        Field sma = new Field(1, 2, "SMI", "Mathematic and Computer Science", "LF", 3);
+    }
+
+    private final static void fillFaculty() {
+        Faculty fs = new Faculty(1, "FS", "Faculty of Science", null);
+    }
+    private final static void fillCities() {
+        City fes = new City(1, "FES", "FES-BOULEMANE", null);
+    }
     
+    public final static void fillMembers() {
+
+    }    
 }

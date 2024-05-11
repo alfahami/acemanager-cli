39fbe0c19a709a7315d39a46b2413b39fe769356
AL-FAHAMI <altoihir@gmail.com>
2024-05-11 16:17:34 +0100

    feat: add list of entries and retrieve list of entries

--- model/manager/Acemanager.java
+++ model/manager/Acemanager.java
@@ -1,17 +1,23 @@
 package model.manager;
 
+import java.util.ArrayList;
+
 import model.Card;
 import model.Member;
 
 public interface Acemanager {
 
     public void addMember(Member newMember);
+    public void addMembers(ArrayList<Member> members);
+    public ArrayList<Member> getMembers();
     public void removeMemeber(int index);
     public void updateMemeber(int index, Member newMember);
     public Member findMember(String passport);
     public Member getMember(int index);
 
     public void addCard(Card card);
+    public void addCards(ArrayList<Card> cards);
+    public ArrayList<Card> getCards();
     public void removeCard(int index);
     public void updateCard(int index, Card newCard);
     public Card findCard(String cin);

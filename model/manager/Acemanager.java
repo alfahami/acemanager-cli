1fc291df6026fdeef14ed75b4bf666326cbbda55
AL-FAHAMI <altoihir@gmail.com>
2024-05-11 20:45:53 +0100

    fix(mod): add return type in crud

--- model/manager/Acemanager.java
+++ model/manager/Acemanager.java
@@ -7,19 +7,19 @@ import model.Member;
 
 public interface Acemanager {
 
-    public void addMember(Member newMember);
+    public boolean addMember(Member newMember);
     public void addMembers(ArrayList<Member> members);
     public ArrayList<Member> getMembers();
-    public void removeMemeber(int index);
-    public void updateMemeber(int index, Member newMember);
+    public Member removeMemeber(int index);
+    public Member updateMemeber(int index, Member newMember);
     public Member findMember(String passport);
     public Member getMember(int index);
 
-    public void addCard(Card card);
+    public boolean addCard(Card card);
     public void addCards(ArrayList<Card> cards);
     public ArrayList<Card> getCards();
-    public void removeCard(int index);
-    public void updateCard(int index, Card newCard);
+    public Card removeCard(int index);
+    public Card updateCard(int index, Card newCard);
     public Card findCard(String cin);
     public Card getCard(int index);
 

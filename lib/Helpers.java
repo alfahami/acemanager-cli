aefc3db2919cfcb26415c64b3e3058a2f8987852
AL-FAHAMI <altoihir@gmail.com>
2024-05-16 18:20:41 +0100

    feat: add printAnyList service

--- lib/Helpers.java
+++ lib/Helpers.java
@@ -4,11 +4,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import manager.Manager;
+import manager.SuperUser;
 import model.Card;
 import model.City;
 import model.Member;
@@ -33,17 +34,17 @@ public class Helpers {
         }
     }
 
-     public static void printAnyArrays(ArrayList<Object> arrays, Manager manager) {
-        if (arrays != null) {
-            String className = arrays.getClass().getCanonicalName();
+     public static void printAnyList(ArrayList<?> list, SuperUser manager) {
+       if (list != null) {
+            String className = list.get(0).getClass().getName();
             // switch array instance
-            // String className = arrays[i].getClass().getName();
+            //String className = list[i].getClass().getName();
             switch (className) {
-                case "ArrayList<Member>":
+                case "model.Member":
                     System.out.println(printTableTitle("LIST OF ACEM MEMBERS"));
                     System.out.print(
-                            "\n  ID | \t   FULL NAME  \t  | AGE | PASSPORT  | MAT AMCI |   CIN    |   REASON   |    CITY    |      FORMATION       |   DIPLOMA   | FACULTY | ADHESION |       EMAIL       |\n ----|--------------------|-----|-----------|----------|----------|-------------|------------|----------------------|-------------|---------|----------|-------------------|\n");
-                    for (int i = 0; i < arrays.size(); i++) {
+                            "\n  ID | \t   FULL NAME  \t  | AGE | PASSPORT  | MAT AMCI |   CIN    |   REASON    |    CITY    |      FORMATION       |   DIPLOMA   | FACULTY | ADHESION |       EMAIL       |\n ----|--------------------|-----|-----------|----------|----------|-------------|------------|----------------------|-------------|---------|----------|-------------------|\n");
+                    for (int i = 0; i < list.size(); i++) {
 
                         Member member = manager.getMember(i);
 
@@ -70,16 +71,16 @@ public class Helpers {
                                         : formatString("No Card!", 10))
 
                                 + "| " + formatString(manager.getCity(member.getIdCity() - 1).getName(), 9) + "| "
-                                + formatString(manager.getCity(member.getIdCity() - 1).getFaculty(member.getIdFaculty() -1).getField(member.getIdField() - 1).getName(), 19)
+                                + formatString(manager.getCity(member.getIdCity() - 1).getFaculty(member.getIdFaculty() -1).getField(member.getIdField() - 1).getAbbr(), 19)
 
                                 + "| " + formatString(manager.getCity(member.getIdCity() - 1).getFaculty(member.getIdFaculty() -1).getField(member.getIdField() - 1).getCertificate(), 10)
                                 + "| "
-                                + formatString((manager.getCity(member.getIdCity() - 1).getFaculty(member.getIdFaculty() -1).getDesc()), 6) +
+                                + formatString((manager.getCity(member.getIdCity() - 1).getFaculty(member.getIdFaculty() -1).getAbbr()), 6) +
 
                                 "| "
                                 + (member.getIsMember() == true ? (formatString("YES ", 6)) : (formatString("NO ", 6)))
                                 + " | " + formatString(member.getEmail(), 16) + "|");
-                        System.out.print(i < arrays.size() - 1
+                        System.out.print(i < list.size() - 1
                                 ? " ----|--------------------|-----|-----------|----------|----------|-------------|------------|----------------------|-------------|---------|----------|-------------------|\n"
                                 : "\n\n");
                     }
@@ -90,12 +91,12 @@ public class Helpers {
                     System.out.println("");
                     System.out.print(
                             "\n\t\t\t\t     ID |\tCITY       |\t\tREGION\t          |   \t\tFACULTIES\t\t |\n\t\t\t\t    ----|------------------|------------------------------|--------------------------------------|\n");
-                    for (int i = 0; i < arrays.length; i++) {
+                    for (int i = 0; i < list.length; i++) {
                         City city = manager.getCity(i);
                         System.out.println("\t\t\t\t   " + printId(i, city.getId()) + " | "
                                 + formatString(city.getName(), 14) + " | " + formatString(city.getRegion(), 26) + " | "
-                                + Arrays.toString(city.getFacultyName()) + "  |");
-                        System.out.print((i < arrays.length - 1)
+                                + list.toString(city.getFacultyName()) + "  |");
+                        System.out.print((i < list.length - 1)
                                 ? "\t\t\t\t    ----|------------------|------------------------------|--------------------------------------|\n"
                                 : "\n\n");
                     }
@@ -104,13 +105,13 @@ public class Helpers {
                     System.out.println(printTableTitle("LIST OF FORMATIONS"));
                     System.out.print(
                             "\n\t\t\t\t\t\t  ID |    FORMATION NAME    | CERTIFICATE    |  DURATION \t  |\n\t\t\t\t\t\t ----|----------------------|----------------|--------------------|\n");
-                    for (int i = 0; i < arrays.length; i++) {
+                    for (int i = 0; i < list.length; i++) {
                         Formation formation = getFormation(i);
                         System.out.println("\t\t\t\t\t\t" + printId(i, formation.getIdFormation()) + " | "
                                 + formatString(formation.getName(), 18) + " | "
                                 + formatString(formation.getFCertificate(), 12) + " | " + formation.getDuration()
                                 + "\t\t  |");
-                        System.out.print((i < arrays.length - 1)
+                        System.out.print((i < list.length - 1)
                                 ? "\t\t\t\t\t\t ----|----------------------|----------------|--------------------|\n"
                                 : "\n\n");
                     }
@@ -119,14 +120,14 @@ public class Helpers {
                     System.out.println(printTableTitle("LIST OF FACULTIES"));
                     System.out.print(
                             "\n\t\t     ID |    FACULTY    | \t\t\t\t\t\t\tFORMATIONS\t\t\t\t\t\t  |\n\t\t    ----|---------------|-----------------------------------------------------------------------------------------------------------------|\n");
-                    for (int i = 0; i < arrays.length; i++) {
+                    for (int i = 0; i < list.length; i++) {
                         FacultyInstitute fac = getFacultyInstitute(i);
                         System.out.println("\t\t   " + printId(i, fac.getIdFacultyInstitute()) + " | "
                                 + formatString(fac.getNameFacInst(), 11) + " | "
-                                + Arrays.toString(fac.getFormationNames()) + " |");
+                                + list.toString(fac.getFormationNames()) + " |");
                         // System.out.println("\t
                         // ----|---------------|------------------------------------------------------------------------------------------------------------------");
-                        System.out.print((i < arrays.length - 1)
+                        System.out.print((i < list.length - 1)
                                 ? "\t\t    ----|---------------|-----------------------------------------------------------------------------------------------------------------|\n"
                                 : "\n\n");
                     }
@@ -135,7 +136,7 @@ public class Helpers {
                     System.out.println(printTableTitle("LIST OF STAY CARDS"));
                     System.out.print(
                             "\n\t\t\t     ID | \tFULL NAME\t  | BIRTH DATE |   CIN    |  STAY REASON | OBTENTION DATE | EXPIRATION DATE |  PIN   |\n\t\t\t    ----|-------------------------|------------|----------|--------------|----------------|-----------------|--------|\n");
-                    for (int i = 0; i < arrays.length; i++) {
+                    for (int i = 0; i < list.length; i++) {
                         Card card = getCard(i);
                         System.out.println("\t\t\t   " + printId(i, card.getIdCard()) + " | "
                                 + formatString(getMemberCard(card.getIdMember()).getFirstName() + " "
@@ -144,18 +145,18 @@ public class Helpers {
                                 + " | " + formatString(card.getPattern(), 10) + " | "
                                 + formatString(card.getObtentionDate(), 12) + " | "
                                 + formatString(card.getExpirationDate(), 13) + " | " + card.getPin() + " |");
-                        System.out.print((i < arrays.length - 1)
+                        System.out.print((i < list.length - 1)
                                 ? "\t\t\t    ----|-------------------------|------------|----------|--------------|----------------|-----------------|--------|\n"
                                 : "\n\n");
                     }
                     break;
                     */
-                default:
-                    System.out.println("The fuck you trying to do though");
+                 default:
+                     System.out.println("The fuck you trying to do though");
             }
-        } else
-            System.exit(0);
-    }
+         } else
+           System.exit(0);
+     }
 
     public static String formatString(String s, int i) {
         String space = " ";

cdf214976db6634fb664ce76d6430d17f94b7273
AL-FAHAMI <altoihir@gmail.com>
2024-05-16 18:20:56 +0100

    test: printAnyList

--- Main.java
+++ Main.java
@@ -1,31 +1,34 @@
+import static lib.Helpers.*;
+
+import java.util.ArrayList;
+
 import constants.FillTables;
 import constants.Role;
-
+import lib.Helpers;
 import manager.Manager;
+import manager.SuperUser;
 import model.City;
+import model.Faculty;
+import model.Field;
+import model.Member;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Manager manager = new Manager();
-        manager.setSession_start();
-
-        System.out.println("\n\nAdmin Session start: " + manager.getSession_start());
+        SuperUser root = new SuperUser();
+        root.setSession_start();
         
-        manager.setRole(Role.ADMINISTRATOR);
+        System.out.println("Root session start: " + root.getSession_start());
+        ArrayList<Field> fields = FillTables.fillFieldsFs();
+        ArrayList<Faculty> facs = FillTables.fillFaculties(fields);
+        root.addCities(FillTables.fillCities(facs));
+        ArrayList<Member> baseMembers = FillTables.fillMembers();
+        root.addMembers(baseMembers);
+        root.addCards(FillTables.fillCards());
 
-        manager.addCities(FillTables.fillCities());
-
-        System.out.println(manager.getCities().toString());
-        System.out.println("\n\n\n");
-        System.out.println(manager.updateCity(3, manager.getCity(5)));
-        System.out.println("\n\n");
-        System.out.println(manager.getCities().toString());
-        Manager manager2 = new Manager();
-        manager2.setSession_start();
-        manager2.setRole(Role.MODERATOR);
-        manager2.addCities(FillTables.fillCities());
+        printAnyList(baseMembers, root);
+        
 
 
     }

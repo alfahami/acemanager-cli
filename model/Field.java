953576c0225f48dea5aed992513142f8b5554fbb
AL-FAHAMI <altoihir@gmail.com>
2024-05-17 17:08:18 +0100

    fix: update toString

--- model/Field.java
+++ model/Field.java
@@ -76,12 +76,7 @@ public class Field {
 
     @Override
     public String toString() {
-        return "{" +
-                " ID='" + id + "'" +
-                ", Name='" + name + "'" +
-                ", Certificate='" + certificate + "'" +
-                ", Duration='" + duration + "'" +
-                "}";
+        return abbr;
     }
 
     @Override

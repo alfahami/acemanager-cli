48412c12a669d715895c1811ec65f43b5fc176c4
AL-FAHAMI <altoihir@gmail.com>
2024-05-21 18:22:09 +0100

    feat: add retrieve member

--- repository/Repository.java
+++ repository/Repository.java
@@ -15,4 +15,10 @@ public class Repository {
     public Member createMember(int id, Member member) {
         return this.mDatastore.put(member.getId(), new Member(member));
     }
+
+    public Member retrievMember(int id) {
+        if(this.mDatastore == null || id > this.mDatastore.size() || id < this.mDatastore.size()) throw new IllegalArgumentException("No member found");
+        return new Member(this.mDatastore.get(id));
+    }
+
 }

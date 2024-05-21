113cef668401d273b2b6d9c8fcebbc1e52ddfd74
AL-FAHAMI <altoihir@gmail.com>
2024-05-21 17:51:23 +0100

    feat: add create Member

--- repository/Repository.java
+++ repository/Repository.java
@@ -1,5 +1,18 @@
 package repository;
 
+
+import java.util.LinkedHashMap;
+
+import pojo.Member;
+
 public class Repository {
-    
+    /*
+     * This is the only class that will have acess to the datastore of our pojos, in a real-production world this would be a server or clouddatabase
+     */
+
+    private LinkedHashMap<Integer, Member> mDatastore = new LinkedHashMap<>();
+
+    public Member createMember(int id, Member member) {
+        return this.mDatastore.put(member.getId(), new Member(member));
+    }
 }

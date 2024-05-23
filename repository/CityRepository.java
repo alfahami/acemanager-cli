9517b132597f81b4d4c548eaae03dcf349943bb8
AL-FAHAMI <altoihir@gmail.com>
2024-05-23 19:23:33 +0100

    feat: add city repository crud

--- repository/CityRepository.java
+++ repository/CityRepository.java
@@ -1,5 +1,27 @@
 package repository;
 
+import java.util.HashMap;
+
+import pojo.City;
+
 public class CityRepository {
     
+        private HashMap<Integer, City> cDatastore = new HashMap<>();
+
+        public City createCity(int id, City city) {
+            return this.cDatastore.put(id, new City(city));
+        }
+
+        public City retrieveCity(int id) {
+            if(this.cDatastore == null || id > this.cDatastore.size() || id < this.cDatastore.size()) throw new IllegalArgumentException("No city found");
+            return new City(this.cDatastore.get(id));
+        }
+
+        public City updateMember(int id, City newCity) {
+            return this.cDatastore.put(id, new City(newCity));
+        }
+
+        public City deleteMember(int id) {
+            return this.cDatastore.remove(id);
+        }
 }

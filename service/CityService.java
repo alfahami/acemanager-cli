33459ebcf27fddefc656a5eb1b61cae814a429f2
AL-FAHAMI <altoihir@gmail.com>
2024-05-26 22:14:17 +0100

    feat: add city service & its impl

--- service/CityService.java
+++ service/CityService.java
@@ -1,5 +1,16 @@
 package service;
 
-public class CityService {
+import java.util.List;
+
+import pojo.City;
+
+public interface CityService {
+
+    public void addCity(City newCity);
+    public void updateCity(City newCity);
+    public City retrieveCity(int id);
+    public List<City> getCityList();
+    public int autoIncrementPK();
+
     
 }
---
+++ service/CityServiceImpl.java
@@ -0,0 +1,44 @@
+package service;
+
+import java.util.List;
+
+import pojo.City;
+import repository.CityRepository;
+
+public class CityServiceImpl implements CityService {
+
+    private CityRepository repository;
+
+
+    public CityServiceImpl(CityRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void addCity(City newCity) {
+        newCity.setId(autoIncrementPK());
+        this.repository.createCity(newCity.getId(), newCity);
+    }
+
+    @Override
+    public void updateCity(City newCity) {
+        this.repository.updateCity(newCity.getId(), newCity);
+    }
+
+    @Override
+    public City retrieveCity(int id) {
+        return new City(this.repository.retrieveCity(id));
+    }
+
+    @Override
+    public List<City> getCityList() {
+        return this.repository.getCityList();
+    }
+
+    @Override
+    public int autoIncrementPK() {
+        if(this.repository.getCityList() != null && this.repository.getCityList().size() > 0) return this.repository.getCityList().size() + 1;
+        else return 1;
+    }
+    
+}

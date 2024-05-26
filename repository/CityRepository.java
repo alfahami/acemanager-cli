8872bd80445661bbea65502392dd554588acedf2
AL-FAHAMI <altoihir@gmail.com>
2024-05-26 22:13:36 +0100

    feat: add city repository

--- repository/CityRepository.java
+++ repository/CityRepository.java
@@ -1,27 +1,34 @@
 package repository;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import pojo.City;
 
 public class CityRepository {
     
-        private HashMap<Integer, City> cDatastore = new HashMap<>();
+        private HashMap<Integer, City> datasource = new HashMap<>();
 
         public City createCity(int id, City city) {
-            return this.cDatastore.put(id, new City(city));
+            return this.datasource.put(id, new City(city));
         }
 
         public City retrieveCity(int id) {
-            if(this.cDatastore == null || id > this.cDatastore.size() || id < this.cDatastore.size()) throw new IllegalArgumentException("No city found");
-            return new City(this.cDatastore.get(id));
+            if(this.datasource == null || id > this.datasource.size() || id < this.datasource.size()) throw new IllegalArgumentException("No city found");
+            return new City(this.datasource.get(id));
         }
 
-        public City updateMember(int id, City newCity) {
-            return this.cDatastore.put(id, new City(newCity));
+        public City updateCity(int id, City newCity) {
+            return this.datasource.put(id, new City(newCity));
         }
 
         public City deleteMember(int id) {
-            return this.cDatastore.remove(id);
+            return this.datasource.remove(id);
+        }
+
+        public List<City> getCityList() {
+            return this.datasource.values().stream()
+                        .sorted((city1, city2) -> Integer.compare(city1.getId(), city2.getId())).collect(Collectors.toList());
         }
 }

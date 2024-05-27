2080cce76700bac779b292173b70bbadfa78c3e2
AL-FAHAMI <altoihir@gmail.com>
2024-05-27 18:46:02 +0100

    feat: full implement session class

--- pojo/Session.java
+++ pojo/Session.java
@@ -1,16 +1,28 @@
 package pojo;
 
+import static helpers.Helpers.*;
+
 public class Session {
     private int id;
     private int idMember;
     private String session_start;
-    private String session_end;   
+    private String session_end;
+
+    public Session(Session source) {
+        setId(source.id);
+        setIdMember(source.idMember);
+        setSession_start(source.session_start);
+        setSession_end(source.session_end);
+
+    }
 
     public int getId() {
         return this.id;
     }
 
     public void setId(int id) {
+        if (id < 0)
+            throw new IllegalArgumentException("Member ID must be greater than 0");
         this.id = id;
     }
 
@@ -22,12 +34,12 @@ public class Session {
         this.idMember = idMember;
     }
 
-
     public String getSession_start() {
         return this.session_start;
     }
 
     public void setSession_start(String session_start) {
+        setDateException(session_start);
         this.session_start = session_start;
     }
 
@@ -36,6 +48,7 @@ public class Session {
     }
 
     public void setSession_end(String session_end) {
+        setDateException(session_end);
         this.session_end = session_end;
     }
 }

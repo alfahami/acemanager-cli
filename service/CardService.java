87eb0c18dbbc7bdb1d9f20ec438721d2e7b13a10
AL-FAHAMI <altoihir@gmail.com>
2024-05-28 20:48:30 +0100

    feat: add card business logic

--- service/CardService.java
+++ service/CardService.java
@@ -1,5 +1,15 @@
 package service;
 
-public class CardService {
-    
+import java.util.List;
+
+import pojo.Card;
+
+public interface CardService {
+    public void addCard(Card newCard);
+    public void updateCard(Card newCard);
+    public void deleteCard(String cin);
+    public Card retrieveCard(String cin);
+    public List<Card> getCardList();
+    public int autoIncrementPK();
+
 }

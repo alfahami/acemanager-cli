576af6cd9fa63c5005d65f614d1b205a88c1314f
AL-FAHAMI <altoihir@gmail.com>
2024-05-28 21:28:25 +0100

    feat: load base cards

--- Main.java
+++ Main.java
@@ -6,12 +6,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import pojo.Card;
+import pojo.City;
 import pojo.Faculty;
 import pojo.Field;
 import pojo.Member;
+import repository.CardRepository;
+import repository.CityRepository;
 import repository.FacultyRepository;
 import repository.FieldRepository;
 import repository.MemberRepository;
+import service.CardService;
+import service.CardServiceImpl;
+import service.CityService;
+import service.CityServiceImpl;
 import service.FacultyService;
 import service.FacultyServiceImpl;
 import service.FieldService;
@@ -33,15 +41,22 @@ public class Main {
     static FacultyRepository facultyRepository = new FacultyRepository();
     static FacultyService facultyService = new FacultyServiceImpl(facultyRepository);
 
+    static CityRepository cityRepository = new CityRepository();
+    static CityService cityService = new CityServiceImpl(cityRepository);
+
+    static CardRepository cardRepository = new CardRepository();
+    static CardService cardService = new CardServiceImpl(cardRepository);
+
     public static void main(String[] args) {
 
         try {
             loadFields();
             loadFaculties();
-            List<Field> fields = facultyService.retrieveFaculty("FS").getFields();
-            System.out.println(fields.toString());
-
+            loadCities();
             loadMembers();
+           loadCards();
+           List<Card> cards = cardRepository.getCardList();
+            System.out.println("hello");
 
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
@@ -65,12 +80,28 @@ public class Main {
         });
     }
 
+    public static void loadCities() throws IOException {
+        Files.lines(paths[2]).forEach(line -> {
+            String[] words = line.split(",");
+            cityService.addCity(new City(Integer.valueOf(words[0]), words[1], words[2],
+                    (ArrayList<Faculty>) facultyService.getFacultyList()));
+        });
+    }
+
     public static void loadMembers() throws IOException {
         Files.lines(paths[3]).forEach(line -> {
             String[] words = line.split(",");
             memberService.addMember(new Member(Integer.valueOf(words[0]), words[1], words[2], words[3], words[4],
                     words[5], Integer.valueOf(words[6]), Integer.valueOf(words[7]), Integer.valueOf(words[8]),
                     Integer.valueOf(words[9]), Integer.valueOf(words[10]), Boolean.valueOf(words[11])));
+        });
+    }
+
+    public static void loadCards() throws IOException {
+        Files.lines(paths[4]).forEach(line -> {
+            String[] words = line.split(",");
+            cardService.addCard(new Card(Integer.valueOf(words[0]), Integer.valueOf(words[1]), Integer.valueOf(words[2]), words[3], words[4], words[5], words[6], words[7]));
+
         });
     }
 }

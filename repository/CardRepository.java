9cc6b5c016586993cab63c293823e6b52db58f18
AL-FAHAMI <altoihir@gmail.com>
2024-05-28 11:52:24 +0100

    feat: add create card impl

---
+++ repository/CardRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import java.util.HashMap;
+
+import pojo.Card;
+
+public class CardRepository {
+    private HashMap<String, Card> datasource = new HashMap<>();
+
+    public Card createCard(Card newCard) {
+        return this.datasource.put(newCard.getCin(), new Card(newCard));
+    }
+}
